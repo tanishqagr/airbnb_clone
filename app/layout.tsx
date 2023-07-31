858392dc7493796a56019892cc37aded645bef93
tanishqagr <31190522+tanishqagr@users.noreply.github.com>
2023-07-31 15:50:20 -0400

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google';
+import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
+import LoginModal from './components/modals/LoginModal';
+import ToasterProvider from './providers/ToasterProvider';
+import getCurrentUser from './actions/getCurrentUser';
+import RentModal from './components/modals/RentModal';
+import SearchModal from './components/modals/SearchModal';
+
+const font = Nunito({ subsets: ['latin'], });
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser  = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <SearchModal />
+          <LoginModal />
+          <RegisterModal />
+          <RentModal />
+          <Navbar currentUser = {currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
